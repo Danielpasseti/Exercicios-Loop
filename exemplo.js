59df6465b5eb5788328e643fef4d978f279b84df
DANIEL DE FREITAS PASSETI PATRICIO <39969184881@info.local>
2024-05-14 08:34:31 -0300

    feat: Ex1

---
+++ exemplo.js
@@ -0,0 +1,46 @@
+//for-loop contato
+// for (let num = 0; num <= 10; num++) {
+//     console.log(num);
+// }
+
+// //contagem regressiva
+// for (let cont = 10; cont >= 0; cont--) {
+//     console.log(cont)
+// }
+
+
+
+
+// //while - loop  condicional
+// let salario = 1000;     
+// while (salario < 5000) {
+//     salario += 100
+//     console.log("O salario ainda e" + salario)
+// }
+
+// let saldo = 1000;
+
+// do {
+//     console.log("Esse e o saldo:" + saldo);
+//     saldo += 100;
+
+// } while (saldo < 5000);
+
+//Array
+// let frutas = ["Banana","Laranja", "Maça", "Pitaya", "Melancia", "Caju"]
+
+// console.log(fruta[3]);
+
+// frutas.forEach(fruta => {
+//     console.log("A fruta é: " + )
+// });
+
+//Percorrendo um lista com for
+//.length => quantidade de elementos dentro de um array
+
+// for(let indice = 0; indice < 5; indice++){
+//     console.log(frutas[indice]);
+//     if(frutas[indice] == "Pitaya"){
+//         console.log("Eu gosto de "+ frutas )
+//     }
+// }
